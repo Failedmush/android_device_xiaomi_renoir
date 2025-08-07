5f58aae44ff7a747ba96ba06867d2f3c2a9a8d1b
Failedmush <163705497+Failedmush@users.noreply.github.com>
2025-08-07 21:02:06 +0900

    Revert miui camera support

--- device.mk
+++ device.mk
@@ -15,10 +15,6 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/audio/mixer_paths_overlay_static.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio/sku_shima/mixer_paths_overlay_static.xml \
     $(LOCAL_PATH)/audio/sound_trigger_mixer_paths_shimaidp.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio/sku_shima/sound_trigger_mixer_paths_shimaidp.xml \
     $(LOCAL_PATH)/audio/sound_trigger_platform_info.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio/sku_shima/sound_trigger_platform_info.xml
-# Camera
-$(call inherit-product-if-exists, vendor/xiaomi/camera/miuicamera.mk)
-ro.miui.notch=1 \
-    ro.product.mod_device=renoir
 
 # NFC
 PRODUCT_COPY_FILES += \
